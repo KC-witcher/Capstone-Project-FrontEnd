4f77c9d7a05e3cb49a3f55f37d49d6e8d43db814
zferguson24 <71412122+zferguson24@users.noreply.github.com>
2022-10-22 16:31:10 -0400

    Buttons on Home Page working, View Projects page created.

--- eplanner/src/components/Home.js
+++ eplanner/src/components/Home.js
@@ -3,22 +3,29 @@ import Nav from './Nav';
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <Stack direction="row" spacing={3}>
       <Nav></Nav>
-      <Stack spacing={2} sx= {{ width: "100%", pr: 3}}>
-        <Box sx={{ pt: 3, borderBottom: 2, borderColor: "lightgray", ml: -3, mr: -3, pl: 3, pr: 3}}> 
+      <Stack spacing={2} sx={{ width: "100%", pr: 3 }}>
+        <Box sx={{ pt: 3, borderBottom: 2, borderColor: "lightgray", ml: -3, mr: -3, pl: 3, pr: 3 }}>
           <h1 className="center">ePlanner Home Page</h1>
-          <br/>
+          <br />
         </Box>
         <Box>
           <h2>Welcome Back, Name</h2>
         </Box>
-        <Button sx={{ p: 2 }} variant="contained">View My Projects</Button>
-        <Button sx={{ p: 2 }} variant="contained">Create New Project</Button>
-        <Button sx={{ p: 2 }} variant="contained">Edit User Information</Button>
+        <Link to="/view" style={{ textDecoration: "none"}}>
+          <Button color="success" sx={{ p: 2, width: "100%" }} variant="contained">View My Projects</Button>
+        </Link>
+        <Link to="/create" style={{ textDecoration: "none"}}>
+          <Button color="success" sx={{ p: 2, width: "100%" }} variant="contained">Create New Project</Button>
+        </Link>
+        <Link to="/edit" style={{ textDecoration: "none"}}>
+          <Button color="success" sx={{ p: 2, width: "100%" }} variant="contained">Edit User Information</Button>
+        </Link>
       </Stack>
     </Stack>
   );
--- eplanner/src/components/ViewProjects.js
+++ eplanner/src/components/ViewProjects.js
@@ -1,10 +1,41 @@
 import '../App.css';
 import Nav from './Nav';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+
+// Need to read this data from the database in the future.
+const projects = ['Project 1', 'Project 2', 'Project 3', 'Project 4', 'Project 5'];
 
 function ViewProjects() {
-  return (
-    <Nav></Nav>
-  );
+    return (
+        <Stack direction="row" spacing={3}>
+            <Nav></Nav>
+            <Stack spacing={2} sx={{ width: "100%", pr: 3 }}>
+                <Box sx={{ pt: 3, borderBottom: 2, borderColor: "lightgray", ml: -3, mr: -3, pl: 3, pr: 3 }}>
+                    <h1 className="center">View My Projects</h1>
+                    <br />
+                </Box>
+                <Box>
+                    <h2>Your Projects</h2>
+                </Box>
+                <Stack spacing={2}>
+                    {projects.map(project => (
+                        <span>
+                            <Button variant="outlined" size="large" style={{ width: "50%" }}>
+                                {project}
+                            </Button>
+                            <IconButton aria-label="delete">
+                                <DeleteIcon color="error"/>
+                            </IconButton>
+                        </span>
+                    ))}
+                </Stack>
+            </Stack>
+        </Stack>
+    );
 }
 
 export default ViewProjects;
