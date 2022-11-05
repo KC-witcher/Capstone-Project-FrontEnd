34700a59af0c71bdbf5417835ad86713a87d2419
B453L <61606627+B453L@users.noreply.github.com>
2022-11-05 17:26:28 -0400

    added initial user authentication and user registration with input checking

--- eplanner/src/components/Login.js
+++ eplanner/src/components/Login.js
@@ -1,29 +1,52 @@
 import "../App.css";
 import Button from "@mui/material/Button";
-import React from "react";
+import React, { useState } from 'react';
 import Stack from "@mui/material/Stack";
 import { TextField } from "@mui/material";
 import { Link } from "react-router-dom";
+import Axios from "axios";
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState ('');
+  const [loginStatus, setLoginStatus] = useState('');
+
+  const login = () => {
+    Axios.post("http://localhost:3002/api/login", {
+       email: email,
+       password: password,
+    }).then((response) => {
+       if (!response.data.message) {
+          setLoginStatus(response.data.message);
+       } else {
+          setLoginStatus (response.data[0].message);
+       }
+    });
+    };
+
   return (
     <Stack sx={{ p: 3, maxWidth: 350 }} spacing={3}>
       <item>
         <h1>Login to ePlanner</h1>
       </item>
       <item>
-        <TextField id="Email" label="Email Address" type="email" placeholder="Enter your Email Address" fullWidth />
+        <TextField id="Email" label="Email Address" type="email" placeholder="Enter your Email Address" onChange={(e) => {
+              setEmail(e.target.value);
+           }} fullWidth />
       </item>
       <item>
-        <TextField id="Password" label="Password" type="password" placeholder="Enter your Password" fullWidth />
+        <TextField id="Password" label="Password" type="password" placeholder="Enter your Password" onChange={(e) => {
+              setPassword(e.target.value);
+           }} fullWidth />
       </item>
       <item>
         <Link to="/home" style={{ textDecoration: "none" }}>
-          <Button variant="contained">Submit</Button>
+          <Button variant="contained" onClick={login}>Submit</Button>
         </Link>
         <Link to="/signup" style={{ textDecoration: "none" }}>
           <Button sx={{ ml: 3 }} variant="outlined">Sign Up</Button>
         </Link>
+        <h1>{loginStatus}</h1>
       </item>
     </Stack>
   );
--- eplanner/src/components/SignUp.js
+++ eplanner/src/components/SignUp.js
@@ -1,34 +1,66 @@
+import React, { useState } from 'react';
 import "../App.css";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { TextField } from "@mui/material";
 import { Link } from "react-router-dom";
+import Axios from "axios";
 
 //After signing up and clicking submit, do we want to go directly to the home page?
 //Or do we want to ask the user to sign in using their new credentials (go to sign in page)?
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState ('');
+  const [passwordAgain, setPasswordAgain] = useState ('');
+  const [fName, setFirstName] = useState('');
+  const [lName, setLastName] = useState ('');
+
+  const signUp = () => {
+    Axios.post("http://localhost:3002/api/create", {
+      email: email,
+      password: password,
+      fName: fName,
+      lNmae: lName,
+     }).then((response) => {
+        console.log(response);
+     });
+   };
+
+   const passwordError = (password !== passwordAgain);
+
   return (
     <Stack sx={{ p: 3, maxWidth: 350 }} spacing={3}>
       <item>
         <h1>Sign Up for ePlanner</h1>
       </item>
+      {/* We can check if we have a valid email address before submitting using regualar expressions or some module we can install */}
       <item>
-        <TextField id="Email" label="Email Address" type="email" placeholder="Enter your Email Address" fullWidth />
+        <TextField id="Email" label="Email Address" type="email" placeholder="Enter your Email Address" onChange={(e) => {
+              setEmail(e.target.value);
+           }} fullWidth />
       </item>
       <item>
-        <TextField id="Password" label="Password" type="password" placeholder="Enter a Password" fullWidth />
+        <TextField id="Password" label="Password" type="password" placeholder="Enter a Password" onChange={(e) => {
+              setPassword(e.target.value);
+           }} fullWidth />
       </item>
       <item>
-        <TextField id="ConfirmPassword" label="Confirm Password" type="password" placeholder="Re-Enter a Password" fullWidth />
+        <TextField id="ConfirmPassword" label="Confirm Password" type="password" placeholder="Re-Enter a Password" onChange={(e) => {
+              setPasswordAgain(e.target.value);
+           }} error={passwordError} helperText={passwordError ? "Passwords do not match, please confirm your password!" : ""} fullWidth />
       </item>
       <item>
-        <TextField id="fName" label="First Name" type="text" placeholder="Enter your First Name" fullWidth />
+        <TextField id="fName" label="First Name" type="text" placeholder="Enter your First Name" onChange={(e) => {
+              setFirstName(e.target.value);
+           }} fullWidth />
       </item>
       <item>
-        <TextField id="lName" label="Last Name" type="text" placeholder="Enter your Last Name" fullWidth />
+        <TextField id="lName" label="Last Name" type="text" placeholder="Enter your Last Name" onChange={(e) => {
+              setLastName(e.target.value);
+           }} fullWidth />
       </item>
       <item>
-        <Button variant="contained">Submit</Button>
+        <Button variant="contained" disabled={email === "" || password === "" || password !== passwordAgain || fName === "" || lName === ""} onClick={signUp}>Submit</Button>
         <Link to="/" style={{ textDecoration: "none" }}>
           <Button sx={{ ml: 3 }} variant="outlined">Return to Login</Button>
         </Link>
