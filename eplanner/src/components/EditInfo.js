7c1fd17f7cea48c2245521ffc8346d8a1ed8412b
B453L <61606627+B453L@users.noreply.github.com>
2022-11-27 15:48:19 -0500

    Update EditInfo.js

--- eplanner/src/components/EditInfo.js
+++ eplanner/src/components/EditInfo.js
@@ -1,31 +1,85 @@
-import '../App.css';
-import Nav from './Nav';
+import "../App.css";
+import React, { useState } from "react";
+import Nav from "./Nav";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { TextField } from "@mui/material";
+import axios from "axios";
 
 function EditInfo() {
-    return (
-        <Stack direction="row" spacing={3} sx={{ height: "100vh" }}>
-            <Nav></Nav>
-            <Stack spacing={2} sx={{ width: "100%", pr: 3 }}>
-                <Box sx={{ pt: 3, borderBottom: 2, borderColor: "lightgray", ml: -3, mr: -3, pl: 3, pr: 3 }}>
-                    <h1 className="center">View My Projects</h1>
-                    <br />
-                </Box>
-                <Box>
-                    <h2>Enter the Information You Would Like to Change</h2>
-                    <br/>
-                    <Stack sx={{ maxWidth: 350 }} spacing={3}>
-                        <TextField id="fName" label="First Name" type="text" placeholder="Enter new First Name" fullWidth InputLabelProps={{ shrink: true }}/>
-                        <TextField id="lName" label="Last Name" type="text" placeholder="Enter new Last Name" fullWidth InputLabelProps={{ shrink: true }}/>
-                    </Stack>
-                    <Button sx={{ mt: 3 }} variant="contained" size="large">Submit</Button>
-                </Box>
-            </Stack>
-        </Stack>
-    );
+  const [fName, setFirstName] = useState("");
+  const [lName, setLastName] = useState("");
+
+  const editInfo = () => {
+    axios
+      .post("http://localhost:3002/api/updateNames", {
+        fname: fName,
+        lname: lName,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  return (
+    <Stack direction="row" spacing={3} sx={{ height: "100vh" }}>
+      <Nav></Nav>
+      <Stack spacing={2} sx={{ width: "100%", pr: 3 }}>
+        <Box
+          sx={{
+            pt: 3,
+            borderBottom: 2,
+            borderColor: "lightgray",
+            ml: -3,
+            mr: -3,
+            pl: 3,
+            pr: 3,
+          }}
+        >
+          <h1 className="center">View My Projects</h1>
+          <br />
+        </Box>
+        <Box>
+          <h2>Enter the Information You Would Like to Change</h2>
+          <br />
+          <Stack sx={{ maxWidth: 350 }} spacing={3}>
+            <TextField
+              id="fName"
+              label="First Name"
+              type="text"
+              placeholder="Enter new First Name"
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+            />
+            <TextField
+              id="lName"
+              label="Last Name"
+              type="text"
+              placeholder="Enter new Last Name"
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+            />
+          </Stack>
+          <Button
+            sx={{ mt: 3 }}
+            variant="contained"
+            size="large"
+            disabled={fName === "" || lName === ""}
+            onClick={editInfo}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Stack>
+    </Stack>
+  );
 }
 
 export default EditInfo;
